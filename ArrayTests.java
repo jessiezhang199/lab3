7f3204ef4a8f0c07cf32e2e2c56dbf4cef077232
jessiezhang199 <jiz199@ucsd.edu>
2023-10-23 15:14:56 -0700

    Update Changes

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,12 +7,25 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
 	}
+  @Test
+  public void Reversed1() {
+    int[] input1 = {1,2};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{2, 1}, input1);
+  }
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = {1,2};
+    assertArrayEquals(new int[]{2,1 }, ArrayExamples.reversed(input1));
   }
 }
